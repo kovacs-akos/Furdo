c67f17dbb2828e4dacaea85609be8796e833812a
kovacs-akos <kovacs.akos@students.jedlik.eu>
2024-11-17 16:19:08 +0100

    Privát tagok getterjei

--- src/GuestActivity.ts
+++ src/GuestActivity.ts
@@ -1,8 +1,24 @@
 export default class GuestActivity{
-    protected guestId: number;
-    protected sectionId: number;
-    protected inOrOut: boolean;
-    protected when: Date;
+    #guestId: number;
+    #sectionId: number;
+    #inOrOut: boolean;
+    #when: Date;
+
+    get guestId(): number {
+        return this.#guestId;
+    }
+
+    get sectionId(): number {
+        return this.#sectionId;
+    }
+
+    get inOrOut(): boolean {
+        return this.#inOrOut;
+    }
+
+    get when(): Date {
+        return this.#when;
+    }
 
     /**
      *
@@ -10,14 +26,14 @@ export default class GuestActivity{
     constructor(line: string) {
         const l: string[] = line.split(" ");
         
-        this.guestId = parseInt(l[0]);
-        this.sectionId = parseInt(l[1]);
-        if (parseInt(l[2]) == 0) this.inOrOut = true;
-        else this.inOrOut =  false;
-        this.when = new Date();
-        this.when.setHours(parseInt(l[3]));
-        this.when.setMinutes(parseInt(l[4]));
-        this.when.setSeconds(parseInt(l[5]));
+        this.#guestId = parseInt(l[0]);
+        this.#sectionId = parseInt(l[1]);
+        if (parseInt(l[2]) == 0) this.#inOrOut = true;
+        else this.#inOrOut =  false;
+        this.#when = new Date();
+        this.#when.setHours(parseInt(l[3]));
+        this.#when.setMinutes(parseInt(l[4]));
+        this.#when.setSeconds(parseInt(l[5]));
      
     }
 
